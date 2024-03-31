c47f6b58ef59b119837c19f5defffa8c5e54983e
Timilehin Olayuwa <timileyinolayuwa@gmail.com>
2024-03-31 16:04:54 +0100

    Theme were decleared and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { Button, ChakraProvider, extendTheme } from '@chakra-ui/react'
 import './App.css'
-
+import Home from './components/Home';
+import { useState } from 'react';
+const customTheme = extendTheme({
+  colors:{
+    primary: "#056608",
+    secondary: "#FFF700",
+    tertiary: "#05660880",
+    defaults: "#000000"
+  }
+})
 function App() {
-
+  const [count, setCount] = useState(0)
+  const handleCount = ()=>{
+    setCount(count => count +1)
+  }
   return (
-    <>
-      
-    </>
+    <ChakraProvider theme={customTheme}>
+      <Home/>
+      <Button bg='primary' color='secondary' _hover={{backgroundColor: "secondary", border: "none", color: "defaults"}} _active={{border: "none"}} onClick={handleCount}> count {count}</Button>
+    </ChakraProvider>
   )
 }
 
